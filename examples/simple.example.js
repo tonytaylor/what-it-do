681bd0873cb49a134059f1820f9643b0f6709ce1
Tony Taylor <tony@tonetaylor.com>
2015-04-07 19:38:22 -0500

    Cleaned up simple example.

--- examples/simple.example.js
+++ examples/simple.example.js
@@ -1,24 +1,31 @@
 /* jshint node:true */
 var r = require('requirejs');
 
-r.config({ nodeRequire: require, paths: { 'fulldeck': '../lib' } });
+r.config({ nodeRequire: require, paths: { 'deck': '../lib' } });
 
-r(['lemonad', 'fulldeck/context', 'fulldeck/role'], function(L, ctx, role) {
-	var data = [
-		{ name: 'Tony Taylor', position: 'Web Application Developer' },
-		{ name: 'Jane Lane', position: 'User Experience Developer' },
-		{ name: 'Marina James', position: 'Product Manager' }
+r(['lemonad', 'deck/context', 'deck/role'], function(L, ctx, role) {
+	var x = [
+		{ name: 'Tony Taylor', title: 'Web Application Developer' },
+		{ name: 'Jane Lane', title: 'User Experience Developer' },
+		{ name: 'Marina James', title: 'Product Manager' }
 	];
 
 	var simpleScenario = ctx(function() {
 		var self = this;
 
-		var employeeAccess = role({
-			'get-name': function() { return this.name; },
-			'get-position': function() { return this.position; }
+		var employeeRecord = role({
+			'getName': function() { return this.name; },
+            'getTitle': function() { return this.title; }
 		});
 
-			console.log(employeeAccess(L.first(self))['get-name']());
-			console.log(employeeAccess(L.last(self))['get-position']());
-	}, data);
+        console.log(employeeRecord(L.first(self.data)).getName());
+        console.log(employeeRecord(L.last(self.data)).getTitle());
+
+        return function() {
+            console.log('%o', L.toArray(arguments));
+            console.log('perform extra stuff here. . .');
+        };
+	}, {data: x});
+
+    simpleScenario('hello?');
 });
